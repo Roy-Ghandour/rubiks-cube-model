9e6cc642c29dd5c92a8a7d955ef1224a71e23ab9
Roy-Ghandour <ghandourroy@gmail.com>
2024-08-05 18:07:16 +0400

    Removed DisplayCube and react dependancy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Color, Face, Cube, applyScramble } from './applyScramble';
 import { solvedCube, SolvedCubeType } from './solvedCube';
-import DisplayCube from './DisplayCube';
 import { generateScramble } from './generateScramble';
 
 export type { Color, Face, Cube, SolvedCubeType };
-export { applyScramble, solvedCube, generateScramble, DisplayCube };
+export { applyScramble, solvedCube, generateScramble };
